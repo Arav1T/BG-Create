043126d53b158236e87bf8ecea81e05722cdc104
Arav1T <aravtiwari127@gmail.com>
2023-06-18 11:43:54 +0530

    first BG Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+import ADD from './Components/ADD';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Custom from './Components/Custom';
+import CreateBlog from './Components/CreateBlog';
+import BlogFormat2 from './Components/BlogFormat/BlogFormat2';
+import BlogFormat1 from './Components/BlogFormat/BlogFormat1';
+import BlogFormat3 from './Components/BlogFormat/BlogFormat3';
+// import Context from './context/Context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    return (
+      <Router>
+      {/* <Context> */}
+      <div>
+        <Navbar />
+        <Routes>
+        <Route element={<Main key="g" category="general"/>} exact path='/'></Route>
+        <Route element={<Main key="b" category="business" />} exact path='/business'></Route>
+        <Route element={<Custom key="h"/>} exact path='/custom'></Route>
+        <Route element={<Main key="e" category="entertainment" />} exact path='/entertainment'></Route>
+       
+        <Route element={<Main key="m" category="technology" />} exact path='/technology'></Route>
+        <Route element={<ADD key="t"/>} exact path='/ADD'></Route>
+        <Route element={<CreateBlog key="u"/>} exact path='/CreateBlog'></Route>
+        <Route element={<BlogFormat1 key="q"/>} exact path='/BlogFormat1'></Route>
+        <Route element={<BlogFormat2 key="e"/>} exact path='/BlogFormat2'></Route>
+        <Route element={<BlogFormat3 key="i"/>} exact path='/BlogFormat3'></Route>
 
-export default App;
+        
+        </Routes>
+      </div>
+      {/* </Context> */}
+      </Router>
+    )
+  }
+
---
+++ src/Components/CreateBlog.js
@@ -0,0 +1,126 @@
+import React, { useContext } from 'react'
+import { CustomData } from '../context/Context'
+import { Link } from 'react-router-dom'
+
+export default function CreateBlog() {
+    const {Blog,BlogImg,setBlogImg,setBlogContent } = useContext(CustomData)
+    // const {  } = useContext()
+
+    const OnChangeImg = (e) => {
+        const file = e.target.files[0];
+        console.log(file)
+        const reader = new FileReader();
+        reader.onload = () => {
+            setBlogImg(reader.result);
+            console.log(reader.result)
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+    const onSubmitImg = () => {
+        // console.log(BlogImg)
+        // console.log("bhug");
+        setBlogContent({title:Blog,description:document.querySelector('#description').value})
+
+    }
+    return (
+        <>
+            <div className='flex justify-between '>
+            <div className='bg-slate-400 mt-10'>
+                <h2 className='text-black'>{Blog}</h2>
+                <div>
+                    <h3>Description</h3>
+                    <textarea className=' border-2 border-black' name="description" id="description" cols="100" rows="10" onChange={e => e.target.value} placeholder='Write Description' ></textarea>
+                </div>
+                <div>
+                    <h4>upload picture</h4>
+                    <input type="file" name="img" id="img" onChange={OnChangeImg} />
+                    {/* <button onClick={onSubmitImg}>submit</button> */}
+                </div>
+            </div>
+            {/* <div>
+                <h4>Chose and click which one eyou like</h4>
+                <select name="BlogFormat" id="BlogFormat" className='bg-slate-400'>
+                <option value="BlogFormat1" className='h-64'><button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}><Link to="/BlogFormat1">
+                    {<div>
+                        <h2>BlogTitle</h2>
+                        <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                        <p>Description</p>
+                    </div>}
+                    
+                </Link></button></option>
+                <option value="BlogFomat2">
+                <button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}>
+                    <Link to="/BlogFormat2">
+                        <div>
+                            <h2>BlogTitle</h2>
+                            <div className='flex'>
+                                <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                                <p>Description</p>
+                            </div>
+                        </div>
+                        
+                    </Link>
+                </button>
+                </option>
+                <option value="BlogFormat3">
+                <button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}>
+                    <Link to="/BlogFormat3">
+                        <div className='flex'>
+                            <div>
+                                <h2>BlogTitle</h2>
+                                <p>Description</p>
+                            </div>
+                            <div >
+                                <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                            </div>
+                        </div>
+                        
+                    </Link>
+                </button>
+                </option>
+            </select>
+            </div>
+            </div> */}
+            <div>
+                <button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}><Link to="/BlogFormat1">
+                    {<div>
+                        <h2>BlogTitle</h2>
+                        <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                        <p>Description</p>
+                    </div>}
+                    
+                </Link></button>
+                <button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}>
+                    <Link to="/BlogFormat2">
+                        <div>
+                            <h2>BlogTitle</h2>
+                            <div className='flex'>
+                                <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                                <p>Description</p>
+                            </div>
+                        </div>
+                        
+                    </Link>
+                </button>
+                <button className='h-36 w-36 border-2 border-black m-7' onClick={onSubmitImg}>
+                    <Link to="/BlogFormat3">
+                        <div className='flex'>
+                            <div>
+                                <h2>BlogTitle</h2>
+                                <p>Description</p>
+                            </div>
+                            <div >
+                                <img src={BlogImg} alt="img" className='h-10 w-10'/>
+                            </div>
+                        </div>
+                        
+                    </Link>
+                </button>
+            <h3>click in any format to create blog</h3>
+            </div>
+            </div>
+        </>
+    )
+}
